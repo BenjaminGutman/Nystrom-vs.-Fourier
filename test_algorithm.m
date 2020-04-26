02c1d50cf8067ed90985c8e51f5c8583b9dd9485
BenjaminGutman <60733209+BenjaminGutman@users.noreply.github.com>
2020-04-26 04:45:12 +0300

    Add files via upload

--- test_algorithm.m
+++ test_algorithm.m
@@ -1,9 +1,9 @@
 % set flag to 0
 tic
 A = [];
-for m=[5 10 20 50 100 200 500]
+for m=[5 10 20 50 100]
     for i=1:10
-        klick_me
+        click_me
         A = [A ; accuracy];
     end
     toc
